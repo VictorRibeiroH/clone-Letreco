844ccd8234c50abddea320e00d8bc6611895349f
unknown <contato.victoribeiro@gmail.com>
2022-02-27 22:57:01 -0300

    att letraa

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const keysSecondRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 const keysThirdRow = ["Z", "X", "C", "V", "B", "N", "M"];
 
 const rows = 6;
-const columns = 6;
+const columns = 5;
 let currentRow = 0;
 let currentColumn = 0;
-let letreco = "VICTOR";
+let letreco = "LAYLA";
 let letrecoMap = {};
 for (let index = 0; index < letreco.length; index++) {
   letrecoMap[letreco[index]] = index;
